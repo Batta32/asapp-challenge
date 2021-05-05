c36d8f21c5d86883a8268682dc4880844d1c1346
Martin Battaglino <martinbatta32@gmail.com>
2021-05-05 20:51:16 -0300

    Configure HealthController

---
+++ src/controllers/health.controller.ts
@@ -0,0 +1,34 @@
+import { Router, Request, Response } from "express";
+import { AppRoute } from "../models/app-route";
+import { Health } from "../models/health";
+
+
+export class HealthController implements AppRoute {
+    public route = "/check";
+    public router: Router = Router();
+
+    // Constructor
+    public constructor() {
+        this.router.get("/", this.check);
+    }
+
+    /**
+     * Checks if the app is running fine.
+     */
+    public check(request: Request, response: Response) {
+        try {
+            const health: Health = new Health();
+            if (health.isAlive()) {
+                response.status(200).send(response.json({
+                    health: "ok"
+                }));
+            } else {
+                throw new Error("There is a roblem in the system");
+            }
+        } catch (err) {
+            response.status(500).json({
+                err: err.message
+            })
+        }
+    }
+}
