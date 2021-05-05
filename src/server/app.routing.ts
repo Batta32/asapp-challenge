113477a59bceb61b5308c2478397a1061e7eefdc
Martin Battaglino <martinbatta32@gmail.com>
2021-05-05 20:52:00 -0300

    Add main classes to route endpoints

---
+++ src/server/app.routing.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { AuthController } from "../controllers/auth.controllers";
+import { HealthController } from "../controllers/health.controller";
+import { MessageController } from "../controllers/message.controller";
+import { UserController } from "../controllers/user.controller";
+import { AppRoute } from "../models/app-route";
+
+export class AppRouting {
+    private route: Router;
+
+    constructor(route: Router) {
+        this.route = route;
+        this.configure()
+    }
+
+    public configure() {
+        // Add the routing classes.
+        this.addRoute([new HealthController(), new UserController(), new AuthController(), new MessageController()]);
+    }
+
+    private addRoute(appRoutes: AppRoute[]) {
+        appRoutes.forEach(route => {
+            this.route.use(route.route, route.router);    
+        });
+    }
+}
