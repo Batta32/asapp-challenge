00881677e87154c527c4b4b483a12e6d0e8ca5d0
Martin Battaglino <martinbatta32@gmail.com>
2021-05-05 20:51:36 -0300

    Configure UserController

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,30 @@
+import { Router, Request, Response } from "express";
+import { AppRoute } from "../models/app-route";
+import { User } from "../models/user";
+
+export class UserController implements AppRoute {
+    public route = "/users";
+    public router: Router = Router();
+
+    // Constructor
+    public constructor() {
+        this.router.get("/", this.createUser);
+    }
+
+    /**
+     * Creates a user.
+     */
+    public async createUser(request: Request, response: Response): Promise<void> {
+        try {
+            const user: User = new User(request.body.username, request.body.password);
+            await user.create();
+            response.status(200).send(response.json({
+                id: user.id
+            }));
+        } catch(err) {
+            response.status(500).json({
+                err: err.message
+            })
+        }
+    }
+}
